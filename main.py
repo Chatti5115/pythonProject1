ef6eb598a9734a088abe1d7c3dc98c932cef080f
Chatti5115 <yujink8114@gmail.com>
2021-08-21 22:58:18 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def print_hi(b):
+    # Use a breakpoint in the code line below to debug your script.
+    a = 10;
+    c = a+b
+    return c
+
+print(print_hi(10))
+
+a = "Hello"
+print(a[0])
+b = a.split("H")
+
+print(b)
+print("\"안녕\" \n 줄바꿈")
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
